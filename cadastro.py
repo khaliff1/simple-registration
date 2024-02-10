e0707872a3d958992861312d42c166962eabe2f6
akima <158107391+khaliff1@users.noreply.github.com>
2024-02-10 14:42:15 -0300

    Add files via upload

---
+++ cadastro.py
@@ -0,0 +1,10 @@
+name = input('what is your name? ')
+age = input('what is your age? ')
+weight = input('how much do you weigh? ')
+day = input('date of birth? ')
+month = input('what is the month of birth? ')
+year = input('what is the year of birth? ')
+print (day,month,year)
+        
+
+
